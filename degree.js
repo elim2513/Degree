6a9a59376fba9911e80fedd2710d9aee12a2d2ec
elim2513 <8elim@stpatrick.org>
2017-02-02 10:53:14 -0600

    Add files via upload
    update degree

---
+++ degree.js
@@ -0,0 +1,25 @@
+const Matrix = require("matrix-js");
+
+let A = Matrix([
+    [0,1,0,1,0],
+    [1,0,1,0,0],
+    [0,1,0,0,0],
+    [1,0,0,0,1],
+    [0,0,0,1,0]
+]);
+//Code here
+function degree(mtrx,v){
+for (var i=0;i<5;i++){
+if(mtrx(v-0)[i]==){
+                  }
+                    }
+
+}
+
+function degreecentrality(mtrx,v){
+
+}
+
+//Challenge
+
+//Run test code with ctrl+shift+p  then type in "script"
